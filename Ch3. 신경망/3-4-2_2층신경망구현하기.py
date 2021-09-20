1158fb36f910675dbb80a5ad21c437417155aa70
자윤 <nyongja@jayun-ui-MacBookAir.local>
2021-09-20 18:20:42 +0900

    3-4-2. 2층 신경망 구현하기

---
+++ Ch3. 신경망/3-4-2_2층신경망구현하기.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def sigmoid(x) :
+    return 1 / (1 + np.exp(-x))
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+print(W1.shape)
+print(X.shape)
+print(B1.shape)
+
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid(A1)
+
+print(A1)
+print(Z1)
